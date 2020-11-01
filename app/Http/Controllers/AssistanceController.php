86079c16c9c74e959e9bfb4a31ce8337dea140d9
Alberto <alberto.martinez@swopyn.com>
2020-11-01 11:26:46 -0600

    !!!^A_PIN Employee Api

--- app/Http/Controllers/AssistanceController.php
+++ app/Http/Controllers/AssistanceController.php
@@ -2,12 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Assistance;
-use App\Employee;
 use App\Exports\AssistancesExport;
 use App\Imports\AssistancesImport;
 use Carbon\Carbon;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -15,7 +12,6 @@ use Illuminate\Support\Facades\Storage;
 use Maatwebsite\Excel\Facades\Excel;
 use PhpOffice\PhpSpreadsheet\Exception;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
-use Throwable;
 
 /**
  * Class AssistanceController
@@ -43,93 +39,6 @@ class AssistanceController extends Controller
             ->paginate(10);
 
         return view('assistance')->with(['assistance' => $assistance]);
-    }
-
-    /**
-     * Get all branches
-     */
-    public function getSucByJson() {
-        $branches = DB::table('sucursales')->get();
-        echo $branches->toJson();
-    }
-
-    /**
-     * Get all employees
-     */
-    public function getEmployeesByJson() {
-        $employees = DB::table('empleados as e')
-            ->join('turnos as t', 'e.id_turno', 't.id')
-            ->select('e.*', 't.nombre_turno as turno')
-            ->get();
-
-        echo $employees->toJson();
-    }
-
-    /**
-     * Save assistance of employee
-     * @param Request $request
-     * @return JsonResponse
-     */
-    public function saveAssistance(Request $request) {
-
-        try {
-
-            $employeeKey = $request->get('key');
-            $coordinates = $request->get('coordinates');
-            $employee = Employee::where('clave', $employeeKey)->first();
-
-            if ($employee){
-
-                $entry = Assistance::where('id_clave', $employee->clave)
-                    ->where('fecha_entrada', Carbon::now()->toDateString())
-                    ->where('entrada', 1)
-                    ->first();
-
-                $complete = Assistance::where('id_clave', $employee->clave)
-                    ->where('fecha_entrada', Carbon::now()->toDateString())
-                    ->where('fecha_salida', Carbon::now()->toDateString())
-                    ->where('entrada', 1)
-                    ->where('salida', 1)
-                    ->first();
-
-                if ($complete) {
-                    return response()->json([
-                        'code' => 200,
-                        'message' => 'El empleado ya cuenta con asistencia.'
-                    ], 201);
-                }
-
-                if ($entry) {
-                    $assistance = Assistance::find($entry->id);
-                    $assistance->asistencia = 1;
-                    $assistance->salida = 1;
-                    $assistance->hora_salida = Carbon::now()->toTimeString();
-                    $assistance->fecha_salida = Carbon::now()->toDateString();
-                    $assistance->save();
-                } else {
-                    $assistance = new Assistance;
-                    $assistance->id_clave = $employee->clave;
-                    $assistance->asistencia = 2;
-                    $assistance->entrada = 1;
-                    $assistance->salida = 0;
-                    $assistance->hora_entrada = Carbon::now()->toTimeString();
-                    $assistance->fecha_entrada = Carbon::now()->toDateString();
-                    $assistance->geolocalizacion = $coordinates;
-                    $assistance->save();
-                }
-            }
-
-            return response()->json([
-                'code' => 201,
-                'message' => 'Registro guardado.'
-            ], 201);
-
-        } catch (Throwable $th) {
-            return response()->json([
-                'code' => 500,
-                'message' => $th->getMessage()
-            ], 500);
-        }
     }
 
     /**
