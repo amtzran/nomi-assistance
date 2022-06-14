ad7e35d21481e81dbc893eb0c16f2d7d91150c47
Alberto Martinez <alberto.martinez@swopyn.com>
2022-06-14 11:31:50 -0500

    Fix total falts

--- app/Exports/AssistancesReportExport.php
+++ app/Exports/AssistancesReportExport.php
@@ -111,7 +111,7 @@ class AssistancesReportExport implements FromCollection, WithStyles
             if (!$dateStart->isSunday()) $diasTrabajados += 1;
 
             $minutosTarde = $hoursInTurn->diffInMinutes($hoursInEmployee);
-            if ($minutosTarde >= 5) $retardos += 1;
+            if ($minutosTarde >= 5 && $isGreatInit) $retardos += 1;
 
             if ($assistance->salida == 0) $faltas += 1;
             if (!$dateStart->isSaturday()) $horasTrabajadas -= 1;
